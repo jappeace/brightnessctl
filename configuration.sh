ba214ecb01ee95de5fe8a5d6cca66852dbb0cc26
Kevin Köllmann <mail@kevinkoellmann.de>
2019-11-16 16:58:25 +0100

    Added a minimum brightness config value (5% of max brightness)

--- configuration.sh
+++ configuration.sh
@@ -3,4 +3,5 @@
 brightFolder="/sys/class/backlight/intel_backlight/"
 currentBright=`cat $brightFolder"brightness"`
 maxBright=`cat $brightFolder"max_brightness"`
+minBright=$((maxBright / 20))
 stepSize=$((maxBright / 10))
